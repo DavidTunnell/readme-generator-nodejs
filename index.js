ef39b7e01cbac9fa6b17d6736d68630a14c5a3eb
David Tunnell <davidtunnell9@gmail.com>
2021-08-03 15:28:33 -0500

    added main questions for user and broke out formatter into a new function

--- index.js
+++ index.js
@@ -1,16 +1,50 @@
-var inquirer = require('inquirer');
+const inquirer = require('inquirer');
 const fs = require('fs');
+
+const fileName = "README_test.md";
 
 inquirer
     .prompt([{
         type: 'input',
-        message: 'What is your user name?',
-        name: 'username',
+        message: 'Provide a title for the project.',
+        name: 'title'
+    }, {
+        type: 'input',
+        message: 'Provide a description of the project.',
+        name: 'description'
+    }, {
+        type: 'input',
+        message: 'Provide installation instructions for the project.',
+        name: 'installation'
+    }, {
+        type: 'input',
+        message: 'Provide usage information for the project.',
+        name: 'usage'
+    }, {
+        type: 'input',
+        message: 'Provide license information for the project.',
+        name: 'license'
+    }, {
+        type: 'input',
+        message: 'Provide contribution guidelines for the project.',
+        name: 'contribution'
+    }, {
+        type: 'input',
+        message: 'Provide test instructions for the project.',
+        name: 'test'
+    }, {
+        type: 'input',
+        message: 'Provide your GitHub user name.',
+        name: 'github'
+    }, {
+        type: 'input',
+        message: 'Provide your email address.',
+        name: 'email'
     }])
     .then((response) => {
         // Use user feedback for... whatever!!
         console.log(response);
-        write(response);
+        writeReadme(response);
     })
     .catch((error) => {
         if (error.isTtyError) {
@@ -20,8 +54,15 @@ inquirer
         }
     });
 
-var write = (response) => {
-    fs.writeFile('log.txt', JSON.stringify(response), (err) =>
+var writeReadme = (response) => {
+    fs.writeFile(fileName, formatContent(response), (err) =>
         err ? console.error(err) : console.log('Success!')
     );
 };
+var formatContent = (response) => {
+    return JSON.stringify(response);
+    //each part of response + make a table of contents
+    //license needs to be 'list of options'
+    //Questions section
+    // badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+}
