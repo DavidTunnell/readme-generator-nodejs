3a825d5e7063aeabd58743ae4a6847daff6214b3
David Tunnell <davidtunnell9@gmail.com>
2021-08-04 14:12:02 -0500

    added all questions, list for license type and rendering of shield.io badge

--- index.js
+++ index.js
@@ -21,9 +21,10 @@ inquirer
         message: 'Provide usage information for the project.',
         name: 'usage'
     }, {
-        type: 'input',
-        message: 'Provide license information for the project.',
-        name: 'license'
+        type: 'list',
+        message: 'Provide which open source license type the project will use.',
+        name: 'license',
+        choices: ['Apache License 2.0', 'BSD 3-Clause "New" or "Revised" license', 'BSD 2-Clause "Simplified" or "FreeBSD" license', 'GNU General Public License (GPL)', 'GNU Library or "Lesser" General Public License (LGPL)', 'MIT license', 'Mozilla Public License 2.0', 'Common Development and Distribution License', 'Eclipse Public License version 2.0']
     }, {
         type: 'input',
         message: 'Provide contribution guidelines for the project.',
@@ -64,6 +65,7 @@ var formatContent = (response) => {
     //each part of response + make a table of contents
     let constructedString = "<h1>" + response.title + "</h1>" + lineBreak +
         "<h2>Description</h2>" + lineBreak +
+        "<p>" + "<img src='https://img.shields.io/badge/Apache-License%202.0%20-Blue'>" + "</p>" +
         "<p>" + response.description + "</p>" + lineBreak +
         "<h2>Table of Contents</h2>" + lineBreak +
         "<ul><li><a href='#installation'>Installation</a></li>" +
@@ -71,8 +73,7 @@ var formatContent = (response) => {
         "<li><a href='#license'>License</a></li>" +
         "<li><a href='#contributing'>Contributing</a></li>" +
         "<li><a href='#tests'>Tests</a></li>" +
-        "<li><a href='#questions'>Questions</a></li>" +
-        "</ul>" +
+        "<li><a href='#questions'>Questions</a></li></ul>" +
         "<h2 id='installation'>Installation</h2>" +
         "<p>" + response.installation + "</p>" + lineBreak +
         "<h2 id='usage'>Usage</h2>" +
@@ -84,7 +85,9 @@ var formatContent = (response) => {
         "<h2 id='tests'>Tests</h2>" +
         "<p>" + response.tests + "</p>" + lineBreak +
         "<h2 id='questions'>Questions</h2>" +
-        "<p>" + response.github + response.email + "</p>" + lineBreak;
+        "<p>Do you have any questions or comments?</p>" +
+        "<ul><li>GitHub Profile: <a href='https://github.com/" + response.github + "'>" + response.github + "</a></li>" +
+        "<li>Email: <a href='mailto: " + response.email + "'>" + response.email + "</a></li></ul>";
     //license needs to be 'list of options'
     //Questions section
     // badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
