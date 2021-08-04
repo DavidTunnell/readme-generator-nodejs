5773164132bbc448c037ef33904fa474269140c2
David Tunnell <davidtunnell9@gmail.com>
2021-08-04 15:10:48 -0500

    testing but I believe full readme is being generated. Added links to license types and link in readme with them

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
+//imports
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+//set file name that will be written
 const fileName = "README_test.md";
 
+//license choices and details
+const licenses = [{ name: 'Apache License 2.0', url: "https://opensource.org/licenses/Apache-2.0" },
+    { name: 'BSD 3-Clause "New" or "Revised" license', url: "https://opensource.org/licenses/BSD-3-Clause" },
+    { name: 'BSD 2-Clause "Simplified" or "FreeBSD license', url: "https://opensource.org/licenses/BSD-2-Clause" },
+    { name: 'GNU General Public License (GPL)', url: "https://opensource.org/licenses/gpl-license" },
+    { name: 'GNU Library or "Lesser" General Public License (LGPL)', url: "https://opensource.org/licenses/lgpl-license" },
+    { name: 'MIT license', url: "https://opensource.org/licenses/MIT" },
+    { name: 'Mozilla Public License 2.0', url: "https://opensource.org/licenses/MPL-2.0" },
+    { name: 'Common Development and Distribution License', url: "https://opensource.org/licenses/CDDL-1.0" },
+    { name: 'Eclipse Public License version 2.0', url: "https://opensource.org/licenses/EPL-2.0" }
+];
+
+//prompt users for questions regarding the readme file generation
 inquirer
     .prompt([{
         type: 'input',
@@ -24,7 +39,7 @@ inquirer
         type: 'list',
         message: 'Provide which open source license type the project will use.',
         name: 'license',
-        choices: ['Apache License 2.0', 'BSD 3-Clause "New" or "Revised" license', 'BSD 2-Clause "Simplified" or "FreeBSD" license', 'GNU General Public License (GPL)', 'GNU Library or "Lesser" General Public License (LGPL)', 'MIT license', 'Mozilla Public License 2.0', 'Common Development and Distribution License', 'Eclipse Public License version 2.0']
+        choices: licenses
     }, {
         type: 'input',
         message: 'Provide contribution guidelines for the project.',
@@ -43,29 +58,32 @@ inquirer
         name: 'email'
     }])
     .then((response) => {
-        // Use user feedback for... whatever!!
-        console.log(response);
+        // format and write file
         writeReadme(fileName, response);
     })
     .catch((error) => {
         if (error.isTtyError) {
-            // Prompt couldn't be rendered in the current environment
+            console.error("Prompt couldn't be rendered in the current environment.");
         } else {
-            // Something else went wrong
+            console.error("Another error occurred. " + error);
         }
     });
 
+//write file to drive in same location as index.js
 var writeReadme = (fileName, response) => {
     fs.writeFile(fileName, formatContent(response), (err) =>
-        err ? console.error(err) : console.log('Successfully printed: ' + fileName)
+        err ? console.error(err) : console.log('Successfully wrote ' + fileName + " in the current directory.")
     );
 };
+
+//construct HTML for the README file
 var formatContent = (response) => {
     const lineBreak = "<br />";
     //each part of response + make a table of contents
+    const licenseUrl = licenses.find(x => x.name === response.license).url;
     let constructedString = "<h1>" + response.title + "</h1>" + lineBreak +
         "<h2>Description</h2>" + lineBreak +
-        "<p>" + "<img src='https://img.shields.io/badge/" + formatForShieldCall(response.license) + "'>" + "</p>" +
+        "<p>" + "<a href='" + licenseUrl + "'><img src='https://img.shields.io/badge/" + formatForShieldCall(response.license) + "'></a></p>" +
         "<p>" + response.description + "</p>" + lineBreak +
         "<h2>Table of Contents</h2>" + lineBreak +
         "<ul><li><a href='#installation'>Installation</a></li>" +
@@ -79,7 +97,7 @@ var formatContent = (response) => {
         "<h2 id='usage'>Usage</h2>" +
         "<p>" + response.usage + "</p>" + lineBreak +
         "<h2 id='license'>License</h2>" +
-        "<p>" + response.license + "</p>" + lineBreak +
+        "<p><a href='" + licenseUrl + "'>" + response.license + "</a></p>" + lineBreak +
         "<h2 id='contributing'>Contributing</h2>" +
         "<p>" + response.contribution + "</p>" + lineBreak +
         "<h2 id='tests'>Tests</h2>" +
@@ -88,13 +106,10 @@ var formatContent = (response) => {
         "<p>Do you have any questions or comments?</p>" +
         "<ul><li>GitHub Profile: <a href='https://github.com/" + response.github + "'>" + response.github + "</a></li>" +
         "<li>Email: <a href='mailto: " + response.email + "'>" + response.email + "</a></li></ul>";
-    //license needs to be 'list of options'
-    //Questions section
-    // badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-
     return constructedString;
 }
 
+//format shield.io badge url from user entry
 var formatForShieldCall = (str) => {
     const arrayOfWords = str.replace(/-/g, ' ').split(" ");
     let formattedString = arrayOfWords[0] + "-";
@@ -102,10 +117,5 @@ var formatForShieldCall = (str) => {
         formattedString += arrayOfWords[i] + "%20";
     }
     formattedString += "-Blue";
-    //first word 
-    //dash -
-
-    //replace all spaces with: %20
-    // dash blue -blue
     return formattedString;
 };
